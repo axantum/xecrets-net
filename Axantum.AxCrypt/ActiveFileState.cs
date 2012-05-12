6f6399bc6a4b0a7c4e3871b53ff1a3f4792925ba
svante <svante@axantum.com>
2012-05-12 15:12:48 +0200

    Proof of concept code for polling and re-encrypting when not locked.

--- Axantum.AxCrypt/ActiveFileState.cs
+++ Axantum.AxCrypt/ActiveFileState.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -37,7 +39,60 @@ namespace Axantum.AxCrypt
 
         private static ActiveFile CheckActiveFileStatus(ActiveFile activeFile)
         {
+            FileInfo activeFileInfo = new FileInfo(activeFile.DecryptedPath);
+            FileStream activeFileStream = null;
+            try
+            {
+                activeFileStream = activeFileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return activeFile;
+            }
+
+            try
+            {
+                if (activeFileInfo.LastWriteTimeUtc > activeFile.LastWriteTimeUtc)
+                {
+                    CopyToWithBackup(activeFileStream, activeFile.EncryptedPath);
+                    activeFile = new ActiveFile(activeFile.EncryptedPath, activeFile.DecryptedPath, activeFileInfo.LastWriteTimeUtc);
+                }
+            }
+            finally
+            {
+                if (activeFileStream != null)
+                {
+                    activeFileStream.Close();
+                }
+            }
+
+            activeFileInfo.Delete();
             return activeFile;
+        }
+
+        private static void CopyToWithBackup(FileStream activeFileStream, string destinationPath)
+        {
+            FileInfo destinationFileInfo = new FileInfo(destinationPath);
+            FileInfo temporaryDestination;
+            do
+            {
+                temporaryDestination = new FileInfo(Path.Combine(destinationFileInfo.DirectoryName, Path.GetRandomFileName()));
+            }
+            while (temporaryDestination.Exists);
+            using (FileStream destinationStream = temporaryDestination.Open(FileMode.CreateNew, FileAccess.ReadWrite, FileShare.None))
+            {
+                activeFileStream.CopyTo(destinationStream);
+            }
+            string backupFilePath;
+            int version = 0;
+            do
+            {
+                backupFilePath = Path.Combine(destinationFileInfo.DirectoryName, destinationFileInfo.Name + "." + version.ToString(CultureInfo.InvariantCulture));
+                ++version;
+            } while (File.Exists(backupFilePath));
+            destinationFileInfo.MoveTo(backupFilePath);
+            temporaryDestination.MoveTo(destinationPath);
+            File.Delete(backupFilePath);
         }
     }
 }
