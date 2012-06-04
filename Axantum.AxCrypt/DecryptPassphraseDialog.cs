da5b638405062c0e6e44a9769e8af78b599cf3cd
svante <svante@axantum.com>
2012-06-04 20:36:12 +0200

    Remove dead code

--- Axantum.AxCrypt/DecryptPassphraseDialog.cs
+++ Axantum.AxCrypt/DecryptPassphraseDialog.cs
@@ -15,13 +15,5 @@ namespace Axantum.AxCrypt
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-        }
     }
 }
