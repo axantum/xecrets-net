fe578ecd6de26da29c7b9707bbc179ea93d1b872
svante <svante@axantum.com>
2012-06-30 22:26:55 +0200

    Added TestCheckActiveFilesSkipIfTooRecent

--- Axantum.AxCrypt.Core.Test/TestFileSystemStateExtensions.cs
+++ Axantum.AxCrypt.Core.Test/TestFileSystemStateExtensions.cs
@@ -89,5 +89,25 @@ namespace Axantum.AxCrypt.Core.Test
             }
             Assert.That(changedWasRaised, Is.False, "The file should be not be detected as decrypted being created because the decrypted file is locked.");
         }
+
+        [Test]
+        public static void TestCheckActiveFilesSkipIfTooRecent()
+        {
+            DateTime utcNow = AxCryptEnvironment.Current.UtcNow;
+            DateTime utcYesterday = utcNow.AddDays(-1);
+            FakeRuntimeFileInfo.AddFile(_encryptedFile1, utcNow, utcNow, utcNow, new MemoryStream(Encoding.UTF8.GetBytes("This is a short file")));
+            FakeRuntimeFileInfo.AddFile(_decryptedFile1, utcYesterday, utcYesterday, utcYesterday, new MemoryStream(Resources.HelloWorld_Key_a_txt));
+
+            ActiveFile activeFile = new ActiveFile(AxCryptEnvironment.Current.FileInfo(_encryptedFile1), AxCryptEnvironment.Current.FileInfo(_decryptedFile1), new AesKey(), ActiveFileStatus.NotDecrypted, null);
+            _fakeRuntimeEnvironment.TimeFunction = (() => { return utcNow.AddSeconds(1); });
+            bool changedWasRaised = false;
+            _fileSystemState.Add(activeFile);
+            _fileSystemState.Changed += ((object sender, EventArgs e) =>
+            {
+                changedWasRaised = true;
+            });
+            _fileSystemState.CheckActiveFiles(ChangedEventMode.RaiseOnlyOnModified, false, new ProgressContext());
+            Assert.That(changedWasRaised, Is.False, "The file should not be detected as decrypted being created, because the current time is too close to LastAccessTimeUtc.");
+        }
     }
 }
