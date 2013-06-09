c85f9082b49fd8efa743900ed6db63992afdf870
Sami Lamti <sami.lamti@gmail.com>
2013-06-09 20:17:55 +0200

    Added a themed file element

---
+++ Axantum.AxCrypt.iOS/Infrastructure/ThemedFileElement.cs
@@ -0,0 +1,45 @@
+using System;
+using MonoTouch.Dialog;
+using System.IO;
+using System.Text;
+using MonoTouch.UIKit;
+
+namespace Axantum.AxCrypt.iOS.Infrastructure
+{
+	public class ThemedFileElement : StyledStringElement
+	{
+		public ThemedFileElement (string fileName, DateTime lastAccessTime) : base(
+			caption: Path.GetFileNameWithoutExtension(fileName),
+			value: String.Concat("Last opened ", FormatDateTime(lastAccessTime)),
+			style: UITableViewCellStyle.Subtitle)
+		{
+		}
+
+		static string FormatDateTime(DateTime value) {
+			DateTime now = DateTime.Now;
+			string nowDateString = now.ToShortDateString ();
+			string yesterdayString = now.AddDays (-1).ToShortDateString ();
+			string valueDateString = value.ToShortDateString ();
+			StringBuilder format = new StringBuilder ();
+
+			if (nowDateString == valueDateString) {
+				format.Append ("today at");
+			} else if (yesterdayString == valueDateString) {
+				format.Append ("yesterday at");
+			} else {
+				format.Append (nowDateString);
+			}
+
+			format.Append (" ").Append (now.ToShortTimeString());
+			return format.ToString ();
+		}
+
+		public override UITableViewCell GetCell (UITableView tv)
+		{
+			var cell = base.GetCell (tv);
+			Theme.Configure (cell);
+			return cell;
+		}
+	}
+}
+
