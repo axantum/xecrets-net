da15bae6156df14146a348425132cbfbf69e4373
svante <svante@axantum.com>
2013-12-20 08:52:40 +0100

    Add tests for ActiveFileComparer

---
+++ Axantum.AxCrypt.Core.Test/TestActiveFileComparer.cs
@@ -0,0 +1,133 @@
+﻿#region Coypright and License
+
+/*
+ * AxCrypt - Copyright 2013, Svante Seleborg, All Rights Reserved
+ *
+ * This file is part of AxCrypt.
+ *
+ * AxCrypt is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AxCrypt is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AxCrypt.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The source is maintained at http://bitbucket.org/axantum/axcrypt-net please visit for
+ * updates, contributions and contact with the author. You may also visit
+ * http://www.axantum.com for more information about the author.
+*/
+
+#endregion Coypright and License
+
+using Axantum.AxCrypt.Core.Crypto;
+using Axantum.AxCrypt.Core.Session;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Axantum.AxCrypt.Core.Test
+{
+    [TestFixture]
+    public static class TestActiveFileComparer
+    {
+        [SetUp]
+        public static void Setup()
+        {
+            SetupAssembly.AssemblySetup();
+        }
+
+        [TearDown]
+        public static void Teardown()
+        {
+            SetupAssembly.AssemblyTeardown();
+        }
+
+        [Test]
+        public static void TestDateComparer()
+        {
+            FakeRuntimeEnvironment.Instance.TimeFunction = () => new DateTime(2013, 01, 01);
+            ActiveFile activeFile1a = new ActiveFile(OS.Current.FileInfo((@"C:\encrypted1.axx")), OS.Current.FileInfo(@"C:\decrypted1.txt"), new AesKey(), ActiveFileStatus.NotDecrypted);
+
+            FakeRuntimeEnvironment.Instance.TimeFunction = () => new DateTime(2013, 01, 01);
+            ActiveFile activeFile1b = new ActiveFile(OS.Current.FileInfo((@"C:\encrypted2.axx")), OS.Current.FileInfo(@"C:\decrypted2.txt"), new AesKey(), ActiveFileStatus.NotDecrypted);
+
+            ActiveFileComparer comparer = ActiveFileComparer.DateComparer;
+            Assert.That(comparer.ReverseSort, Is.False);
+
+            Assert.That(comparer.Compare(activeFile1a, activeFile1b), Is.EqualTo(0));
+            Assert.That(comparer.Compare(activeFile1b, activeFile1a), Is.EqualTo(0));
+            comparer.ReverseSort = true;
+            Assert.That(comparer.Compare(activeFile1a, activeFile1b), Is.EqualTo(0));
+            Assert.That(comparer.Compare(activeFile1b, activeFile1a), Is.EqualTo(0));
+            comparer.ReverseSort = false;
+
+            FakeRuntimeEnvironment.Instance.TimeFunction = () => new DateTime(2013, 01, 02);
+            ActiveFile activeFile2 = new ActiveFile(OS.Current.FileInfo((@"C:\encrypted3.axx")), OS.Current.FileInfo(@"C:\decrypted3.txt"), new AesKey(), ActiveFileStatus.NotDecrypted);
+
+            Assert.That(comparer.Compare(activeFile1a, activeFile2), Is.LessThan(0));
+            Assert.That(comparer.Compare(activeFile2, activeFile1a), Is.GreaterThan(0));
+            comparer.ReverseSort = true;
+            Assert.That(comparer.Compare(activeFile1a, activeFile2), Is.GreaterThan(0));
+            Assert.That(comparer.Compare(activeFile2, activeFile1a), Is.LessThan(0));
+            comparer.ReverseSort = false;
+        }
+
+        [Test]
+        public static void TestEncryptedNameComparer()
+        {
+            ActiveFile activeFile1a = new ActiveFile(OS.Current.FileInfo((@"C:\encrypted1.axx")), OS.Current.FileInfo(@"C:\decrypted1a.txt"), new AesKey(), ActiveFileStatus.NotDecrypted);
+            ActiveFile activeFile1b = new ActiveFile(OS.Current.FileInfo((@"C:\encrypted1.axx")), OS.Current.FileInfo(@"C:\decrypted1b.txt"), new AesKey(), ActiveFileStatus.NotDecrypted);
+
+            ActiveFileComparer comparer = ActiveFileComparer.EncryptedNameComparer;
+            Assert.That(comparer.ReverseSort, Is.False);
+
+            Assert.That(comparer.Compare(activeFile1a, activeFile1b), Is.EqualTo(0));
+            Assert.That(comparer.Compare(activeFile1b, activeFile1a), Is.EqualTo(0));
+            comparer.ReverseSort = true;
+            Assert.That(comparer.Compare(activeFile1a, activeFile1b), Is.EqualTo(0));
+            Assert.That(comparer.Compare(activeFile1b, activeFile1a), Is.EqualTo(0));
+            comparer.ReverseSort = false;
+
+            ActiveFile activeFile2 = new ActiveFile(OS.Current.FileInfo((@"C:\encrypted2.axx")), OS.Current.FileInfo(@"C:\decrypted1a.txt"), new AesKey(), ActiveFileStatus.NotDecrypted);
+
+            Assert.That(comparer.Compare(activeFile1a, activeFile2), Is.LessThan(0));
+            Assert.That(comparer.Compare(activeFile2, activeFile1a), Is.GreaterThan(0));
+            comparer.ReverseSort = true;
+            Assert.That(comparer.Compare(activeFile1a, activeFile2), Is.GreaterThan(0));
+            Assert.That(comparer.Compare(activeFile2, activeFile1a), Is.LessThan(0));
+            comparer.ReverseSort = false;
+        }
+
+        [Test]
+        public static void TestDecryptedNameComparer()
+        {
+            ActiveFile activeFile1a = new ActiveFile(OS.Current.FileInfo((@"C:\encrypted1a.axx")), OS.Current.FileInfo(@"C:\decrypted1.txt"), new AesKey(), ActiveFileStatus.NotDecrypted);
+            ActiveFile activeFile1b = new ActiveFile(OS.Current.FileInfo((@"C:\encrypted1b.axx")), OS.Current.FileInfo(@"C:\decrypted1.txt"), new AesKey(), ActiveFileStatus.NotDecrypted);
+
+            ActiveFileComparer comparer = ActiveFileComparer.DecryptedNameComparer;
+            Assert.That(comparer.ReverseSort, Is.False);
+
+            Assert.That(comparer.Compare(activeFile1a, activeFile1b), Is.EqualTo(0));
+            Assert.That(comparer.Compare(activeFile1b, activeFile1a), Is.EqualTo(0));
+            comparer.ReverseSort = true;
+            Assert.That(comparer.Compare(activeFile1a, activeFile1b), Is.EqualTo(0));
+            Assert.That(comparer.Compare(activeFile1b, activeFile1a), Is.EqualTo(0));
+            comparer.ReverseSort = false;
+
+            ActiveFile activeFile2 = new ActiveFile(OS.Current.FileInfo((@"C:\encrypted1a.axx")), OS.Current.FileInfo(@"C:\decrypted2.txt"), new AesKey(), ActiveFileStatus.NotDecrypted);
+
+            Assert.That(comparer.Compare(activeFile1a, activeFile2), Is.LessThan(0));
+            Assert.That(comparer.Compare(activeFile2, activeFile1a), Is.GreaterThan(0));
+            comparer.ReverseSort = true;
+            Assert.That(comparer.Compare(activeFile1a, activeFile2), Is.GreaterThan(0));
+            Assert.That(comparer.Compare(activeFile2, activeFile1a), Is.LessThan(0));
+            comparer.ReverseSort = false;
+        }
+    }
+}
