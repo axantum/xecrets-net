912729bb5c58b1fb78f41fa626fbf70cc002b907
Sami Lamti <sami.lamti@gmail.com>
2014-03-08 10:51:51 +0100

    Removed application code against the old framework

--- Axantum.AxCrypt.Mac/AppDelegate.cs
+++ Axantum.AxCrypt.Mac/AppDelegate.cs
@@ -1,28 +1,18 @@
 using System;
-using System.Drawing;
 using MonoMac.Foundation;
 using MonoMac.AppKit;
-using Axantum.AxCrypt.Core.IO;
 using Axantum.AxCrypt.Core;
-using System.IO;
-using Axantum.AxCrypt.Core.Crypto;
 using Axantum.AxCrypt.Core.UI;
-using Axantum.AxCrypt.Core.Runtime;
 using System.Diagnostics;
-using Axantum.AxCrypt.Mono;
-using Axantum.AxCrypt.Mac.Windows;
 
 namespace Axantum.AxCrypt.Mac
 {
 	public partial class AppDelegate : NSApplicationDelegate
 	{
 		NSWindowController mainWindowController;
-		OpenFileFromFinderController openFile;
 
 		public AppDelegate ()
 		{
-			OS.Current = new AxCrypt.Core.MacOsx.RuntimeEnvironment();
-
 			UpdateCheck updatecheck = new UpdateCheck(UpdateCheck.VersionUnknown);
 			Uri restApiUri = new Uri("https://www.axantum.com/Xecrets/RestApi.ashx/axcrypt2version/mac");
 			Uri versionUri = new Uri("http://www.axantum.com/");
@@ -47,67 +37,7 @@ namespace Axantum.AxCrypt.Mac
 
 		public override void FinishedLaunching (NSObject notification)
 		{
-			AppController.Initialize ();
-			mainWindowController = new MainWindowController ();
-			mainWindowController.ShowWindow (this);
-			if (openFile != null)
-				mainWindowController.Window.Miniaturize (this);
-			else {
-				if (VersionInformationWindowController.ShouldShowVersionInformation) {
-					VersionInformationWindowController versionInfo = new VersionInformationWindowController ();
-					versionInfo.ShowWindow (this);
-				}
-			}
 		}
-
-		partial void about (NSObject sender)
-		{
-			AppController.About(sender);
-		}
-
-		partial void view (NSObject sender)
-		{
-			AppController.DecryptAndOpenFile(new ProgressContext(), AppController.OperationFailureHandler);
-		}
-
-		partial void onlineHelp (NSObject sender)
-		{
-			AppController.OnlineHelp();
-		}
-
-		partial void encrypt (NSObject sender)
-		{
-			AppController.EncryptFile(new ProgressContext(), AppController.OperationFailureHandler);
-		}
-
-		partial void decrypt (NSObject sender)
-		{
-			AppController.DecryptAndOpenFile(new ProgressContext(), AppController.OperationFailureHandler);
-		}
-
-		public override void OpenFiles (NSApplication sender, string[] filenames)
-		{
-			openFile = new OpenFileFromFinderController ();
-			openFile.UserChoseOpen += (string passphrase) => {
-				AppController.DecryptAndOpenFile(
-					OS.Current.FileInfo(filenames[0]),
-					new Passphrase(passphrase),
-					new ProgressContext(),
-					AppController.OperationFailureHandler);
-				ReleaseOpenFileController();
-			};
-			openFile.UserChoseCancel += () => {
-				ReleaseOpenFileController();
-			};
-			openFile.ShowWindow (sender);
-		}
-
-		void ReleaseOpenFileController ()
-		{
-			openFile.Close ();
-			openFile.Dispose ();
-			openFile = null;
-		}
-	}
+    }
 }
 
