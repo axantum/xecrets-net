ef647ca56af3f722190658a8fb4dd714cbf8302c
svante <svante@axantum.com>
2014-05-19 19:48:36 +0200

    Add regression tests for V2 AES-128 decryption

--- Axantum.AxCrypt.Core.Test/TestV2RegressionCompleteFiles.cs
+++ Axantum.AxCrypt.Core.Test/TestV2RegressionCompleteFiles.cs
@@ -12,7 +12,7 @@ using System.Security.Cryptography;
 namespace Axantum.AxCrypt.Core.Test
 {
     [TestFixture]
-    public static class TestV2AES256RegressionCompleteFiles
+    public static class TestV2RegressionCompleteFiles
     {
         private static readonly string _rootPath = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
 
@@ -29,28 +29,46 @@ namespace Axantum.AxCrypt.Core.Test
         }
 
         [Test]
-        public static void TestSimpleSmallFile()
+        public static void TestSimpleSmallFile256()
         {
             TestOneFile("short-txt-AES256.axx", "PâsswördètMëd§½ Lôñg|´¨", "2de4823aa40ed2a6d040e7ba67bf60e3b1ae5c1f1bc2391ba8435ec7d1597f49");
         }
 
         [Test]
-        public static void TestLargerUncompressibleFile()
+        public static void TestLargerUncompressibleFile256()
         {
             TestOneFile("snow-jpg-AES256.axx", "PâsswördètMëd§½ Lôñg|´¨", "b541684642894f9385b15ddd62f980e20a730fc036bcb1bbb4bad75b1f4889b4");
         }
 
         [Test]
-        public static void TestLargerCompressibleTextFile()
+        public static void TestLargerCompressibleTextFile256()
         {
             TestOneFile("Frankenstein-txt-AES256.axx", "PâsswördètMëd§½ Lôñg|´¨", "3493994a1a7d891e1a6fb4e3f60c58cbfb3e6f71f12f4c3ffe51c0c9498eb520");
         }
 
-        private static void TestOneFile(string resourceName, string password, string sha256HashValue)
+        [Test]
+        public static void TestSimpleSmallFile128()
+        {
+            TestV2RegressionCompleteFiles.TestOneFile("short-txt-V2AES128.axx", "PâsswördètMëd§½ Lôñg|´¨", "2de4823aa40ed2a6d040e7ba67bf60e3b1ae5c1f1bc2391ba8435ec7d1597f49");
+        }
+
+        [Test]
+        public static void TestLargerUncompressibleFile128()
+        {
+            TestV2RegressionCompleteFiles.TestOneFile("snow-jpg-V2AES128.axx", "PâsswördètMëd§½ Lôñg|´¨", "b541684642894f9385b15ddd62f980e20a730fc036bcb1bbb4bad75b1f4889b4");
+        }
+
+        [Test]
+        public static void TestLargerCompressibleTextFile128()
+        {
+            TestV2RegressionCompleteFiles.TestOneFile("Frankenstein-txt-V2AES128.axx", "PâsswördètMëd§½ Lôñg|´¨", "3493994a1a7d891e1a6fb4e3f60c58cbfb3e6f71f12f4c3ffe51c0c9498eb520");
+        }
+
+        internal static void TestOneFile(string resourceName, string password, string sha256HashValue)
         {
             string source = Path.Combine(_rootPath, "source.axx");
             string destination = Path.Combine(_rootPath, "destination.file");
-            Stream stream = Assembly.GetAssembly(typeof(TestV2AES256RegressionCompleteFiles)).GetManifestResourceStream("Axantum.AxCrypt.Core.Test.resources." + resourceName);
+            Stream stream = Assembly.GetAssembly(typeof(TestV2RegressionCompleteFiles)).GetManifestResourceStream("Axantum.AxCrypt.Core.Test.resources." + resourceName);
             FakeRuntimeFileInfo.AddFile(source, FakeRuntimeFileInfo.TestDate1Utc, FakeRuntimeFileInfo.TestDate2Utc, FakeRuntimeFileInfo.TestDate3Utc, stream);
 
             Passphrase passphrase = new Passphrase(password);
