74a68447092b8b3788ad6040c1aff21e4fa21ac5
Svante Seleborg <svante@axantum.com>
2015-09-10 12:11:29 +0200

    Add skeleton for online API integration with AxCrypt server

---
+++ Axantum.AxCrypt.Api/AxCryptApiClient.cs
@@ -0,0 +1,18 @@
+﻿using Axantum.AxCrypt.Api.Model;
+using Axantum.AxCrypt.Core.Crypto;
+using Axantum.AxCrypt.Core.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Axantum.AxCrypt.Api
+{
+    public class AxCryptApiClient
+    {
+        public UserSummary User(EmailAddress email, Passphrase passphrase)
+        {
+            return null;
+        }
+    }
+}
---
+++ Axantum.AxCrypt.Api/Model/UserSummary.cs
@@ -0,0 +1,46 @@
+﻿using Axantum.AxCrypt.Core.Crypto.Asymmetric;
+using Axantum.AxCrypt.Core.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Axantum.AxCrypt.Api.Model
+{
+    /// <summary>
+    /// A summary of information we know about a user, typically fetched at the start of a session.
+    /// </summary>
+    public class UserSummary
+    {
+        public UserSummary()
+        {
+            EmailAddress = EmailAddress.Empty;
+            PublicKeyThumbprints = new PublicKeyThumbprint[0];
+            SubscriptionLevel = SubscriptionLevel.None;
+        }
+
+        /// <summary>
+        /// Gets the email address of the user.
+        /// </summary>
+        /// <value>
+        /// The email address.
+        /// </value>
+        public EmailAddress EmailAddress { get; private set; }
+
+        /// <summary>
+        /// Gets the known public key thumbprints for this user.
+        /// </summary>
+        /// <value>
+        /// The public key thumbprints without any particular order.
+        /// </value>
+        public IEnumerable<PublicKeyThumbprint> PublicKeyThumbprints { get; private set; }
+
+        /// <summary>
+        /// Gets the currently valid subscription level.
+        /// </summary>
+        /// <value>
+        /// The subscription level.
+        /// </value>
+        public SubscriptionLevel SubscriptionLevel { get; private set; }
+    }
+}
