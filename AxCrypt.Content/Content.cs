f12779c06e69ca2feb69d0084ffdc96be83d2384
svante@axantum.com <svante@axantum.com>
2015-12-08 08:43:20 +0100

    Move long texts into LongResources to make editing more convenient.

--- AxCrypt.Content/Content.cs
+++ AxCrypt.Content/Content.cs
@@ -9,5 +9,24 @@ namespace AxCrypt.Content
 {
     public class Content : Resources
     {
+        public string Template_ActivateEmail_cshtml { get { return LongResources.Template_ActivateEmail_cshtml; } }
+
+        public string Template_AppActivateEmail_cshtml { get { return LongResources.Template_AppActivateEmail_cshtml; } }
+
+        public string Template_AppActivateEmailPlainText_cshtml { get { return LongResources.Template_AppActivateEmailPlainText_cshtml; } }
+
+        public string Template_ChangeEmailConfirmEmail_cshtml { get { return LongResources.Template_ChangeEmailConfirmEmail_cshtml; } }
+
+        public string Template_ChangeEmailExistEmail_cshtml { get { return LongResources.Template_ChangeEmailExistEmail_cshtml; } }
+
+        public string Template_EmailLayout_cshtml { get { return LongResources.Template_EmailLayout_cshtml; } }
+
+        public string Template_InvitationEmail_cshtml { get { return LongResources.Template_InvitationEmail_cshtml; } }
+
+        public string Template_InvitationEmailPlainText_cshtml { get { return LongResources.Template_InvitationEmailPlainText_cshtml; } }
+
+        public string Template_PasswordResetEmail_cshtml { get { return LongResources.Template_PasswordResetEmail_cshtml; } }
+
+        public string Template_UnregisterEmail_cshtml { get { return LongResources.Template_UnregisterEmail_cshtml; } }
     }
 }
