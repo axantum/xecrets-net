9af9b58443296371a0ae371f6aff2addcaf02e24
Svante Seleborg <svante.seleborg@axcrypt.net>
2016-10-16 18:50:33 +0200

    Support whatip REST API

---
+++ Axantum.AxCrypt.Api/Response/WhatIpResponse.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axantum.AxCrypt.Api.Response
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class WhatIpResponse : ResponseBase
+    {
+        public WhatIpResponse()
+            : this(string.Empty)
+        {
+        }
+
+        public WhatIpResponse(string ipAddress)
+        {
+            Message = "OK";
+            IpAddress = ipAddress;
+        }
+
+        [JsonProperty("Ip")]
+        public string IpAddress { get; }
+    }
+}
