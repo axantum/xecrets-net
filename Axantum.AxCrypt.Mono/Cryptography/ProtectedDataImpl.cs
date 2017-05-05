675bb4e5376a46b9990a2cd03264051007701838
Svante Seleborg <svante.seleborg@axcrypt.net>
2017-05-05 09:39:35 +0200

    #61: Code Maintenance Refactor IProtectedData and ProtectedDataImpl from Axantum.Xecrets.Core to Axantum.AxCrypt.Abstractions and Axantum.AxCrypt.Mono respectivelym

---
+++ Axantum.AxCrypt.Mono/Cryptography/ProtectedDataImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using Axantum.AxCrypt.Abstractions;
+
+namespace Axantum.AxCrypt.Mono
+{
+    public class ProtectedDataImpl : IProtectedData
+    {
+        #region IProtectedData Members
+
+        public byte[] Protect(byte[] userData, byte[] optionalEntropy)
+        {
+            return ProtectedData.Protect(userData, optionalEntropy, System.Security.Cryptography.DataProtectionScope.LocalMachine);
+        }
+
+        public byte[] Unprotect(byte[] encryptedData, byte[] optionalEntropy)
+        {
+            return ProtectedData.Unprotect(encryptedData, optionalEntropy, System.Security.Cryptography.DataProtectionScope.LocalMachine);
+        }
+
+        #endregion IProtectedData Members
+    }
+}
