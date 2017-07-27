ffd10ffa5062ee54b216902bec4559ee0d4ed298
Svante Seleborg <svante.seleborg@axcrypt.net>
2017-07-27 17:30:01 +0200

    #316: Code changes to support features in other repositories https://bitbucket.org/axcryptab/axcrypt-xwt/issues/114/re-implement-modalwindow-to-use-dialog Add a TaskRunner synchronous wrapper for async code in very special situations.

---
+++ Axantum.AxCrypt.Common/TaskRunner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axantum.AxCrypt.Common
+{
+    /// <summary>
+    /// https://stackoverflow.com/questions/40324300/calling-async-methods-from-non-async-code (Stephen Cleary)
+    /// </summary>
+    public class TaskRunner
+    {
+        private Func<Task> _task;
+
+        private TaskRunner(Func<Task> task)
+        {
+            _task = task;
+        }
+
+        public static void WaitFor(Func<Task> task)
+        {
+            new TaskRunner(task).Wait();
+        }
+
+        private void Wait()
+        {
+            Task.Run(_task).GetAwaiter().GetResult();
+        }
+    }
+}
