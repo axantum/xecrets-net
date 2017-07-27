00185d30ab5d9094617e84d8f2029f07d4da73f4
Svante Seleborg <svante.seleborg@axcrypt.net>
2017-07-27 17:38:20 +0200

    #316: Code changes to support features in other repositories https://bitbucket.org/axcryptab/axcrypt-xwt/issues/114/re-implement-modalwindow-to-use-dialog Add some serious warning comments.

--- Axantum.AxCrypt.Common/TaskRunner.cs
+++ Axantum.AxCrypt.Common/TaskRunner.cs
@@ -18,6 +18,17 @@ namespace Axantum.AxCrypt.Common
             _task = task;
         }
 
+        /// <summary>
+        /// Waits for a Task-returning function, by running it on a thread pool thread.
+        /// </summary>
+        /// <param name="task">The task.</param>
+        /// <remarks>
+        /// This is DANGEROUS and should be avoided. The Function MUST be able to run on a thread pool thread,
+        /// and MUST NOT invoke code on the UI-thread (strictly speaking the caller thread) since that may
+        /// cause a deadlock. Only use this as a last resort when under restrictions by external code that cannot
+        /// be updated to async. Never use this if the code can be changed to async, or an async API added, no
+        /// matter how much work it is.
+        /// </remarks>
         public static void WaitFor(Func<Task> task)
         {
             new TaskRunner(task).Wait();
