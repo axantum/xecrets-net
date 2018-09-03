d2a4a4bb09f6482d7babd8a6960524d0d02a6a31
Svante Seleborg <svante.seleborg@axcrypt.net>
2018-09-03 23:01:29 +0200

    #429: Implement public SDK API (WIP) First parts of a public SDK API.

---
+++ AxCrypt.Sdk/SdkRandom.cs
@@ -0,0 +1,41 @@
+﻿using Axantum.AxCrypt.Abstractions.Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Axantum.AxCrypt.Abstractions.TypeResolve;
+
+namespace AxCrypt.Sdk
+{
+    /// <summary>
+    /// Random helpers
+    /// </summary>
+    public class SdkRandom
+    {
+        private RandomNumberGenerator _rng;
+
+        public SdkRandom()
+        {
+            _rng = New<RandomNumberGenerator>();
+        }
+
+        public string Password(int bits)
+        {
+            if (bits < 1)
+            {
+                throw new ArgumentException($"{nameof(bits)} must be greater than 0.");
+            }
+            int bytes = (bits - 1) / 8 + 1;
+
+            byte[] data = new byte[bytes];
+            _rng.GetBytes(data);
+
+            string password = Convert.ToBase64String(data);
+            while (password.EndsWith("="))
+            {
+                password = password.Substring(0, password.Length - 1);
+            }
+
+            return password;
+        }
+    }
+}
