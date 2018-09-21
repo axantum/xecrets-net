cb24847d7bb364a5d9786dcc8276201843040630
Prabhukumar R <Prabhukumar R@localhost>
2018-09-21 19:47:18 +0530

    #297: Add structural integrity check of encrypted file.

--- Axantum.AxCrypt.Core/UI/FileOperationsController.cs
+++ Axantum.AxCrypt.Core/UI/FileOperationsController.cs
@@ -28,9 +28,7 @@
 using Axantum.AxCrypt.Abstractions;
 using Axantum.AxCrypt.Core.Crypto;
 using Axantum.AxCrypt.Core.Extensions;
-using Axantum.AxCrypt.Core.Header;
 using Axantum.AxCrypt.Core.IO;
-using Axantum.AxCrypt.Core.Reader;
 using Axantum.AxCrypt.Core.Runtime;
 using Axantum.AxCrypt.Core.Session;
 using Axantum.AxCrypt.Core.UI.ViewModel;
