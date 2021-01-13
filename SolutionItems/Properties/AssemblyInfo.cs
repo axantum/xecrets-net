843729fbaf096305d0b20d328c4c17db93e6f801
Prabhukumar R <prabhukumar@axcrypt.net>
2021-01-13 12:28:22 +0530

    AXC-757: Update copyright year in about
    Copyright year updated from 2012-2020 to 2012-2021

--- SolutionItems/Properties/AssemblyInfo.cs
+++ SolutionItems/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("AxCrypt AB")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyProduct("AxCrypt")]
-[assembly: AssemblyCopyright("Copyright © 2012-2020 AxCrypt AB")]
+[assembly: AssemblyCopyright("Copyright © 2012-2021 AxCrypt AB")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
